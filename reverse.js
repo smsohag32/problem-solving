b4d9d1641a65ae97d7273802d1fd703c43899885
smsohag32 <sohagsheik32@gmail.com>
2025-01-31 21:22:16 +0600

    reverse problem solved

--- reverse.js
+++ reverse.js
@@ -2,10 +2,14 @@ const reverse = function (x) {
    let t = 0;
    while (x !== 0) {
       let c = x % 10;
-      console.log(c);
+      t = t * 10 + c;
+      console.log("result of t expect ", t);
+      x = Math.floor(x / 10);
+
+      console.log(x, t);
+      // return t;
    }
+   return t;
 };
 
-const r = reverse(999);
-
-
+const r = reverse(56);
